22d1b122febc2adb4c8edc1368f0c5110727f522
Divy <divyparekh1810@gmail.com>
2022-11-26 16:44:19 +0530

    Done with the start and reset function

--- stop-watch/src/App.jsx
+++ stop-watch/src/App.jsx
@@ -13,13 +13,60 @@ const App = () => {
     milliSecond: 0,
   });
 
-  // let status = 0;
+  const [interv, setInterv] = useState();
+
+  const start = () => {
+    run();
+    setInterv(setInterval(run, 10));
+  };
+
+  let updatedHour = time.hour;
+  let updatedMinute = time.minute;
+  let updatedSecond = time.second;
+  let updatedMilliSecond = time.milliSecond;
+
+  const run = () => {
+    if (updatedMinute === 60) {
+      updatedHour++;
+      updatedMinute = 0;
+    }
+
+    if (updatedSecond === 60) {
+      updatedMinute++;
+      updatedSecond = 0;
+    }
+
+    if (updatedMilliSecond === 60) {
+      updatedSecond++;
+      updatedMilliSecond = 0;
+    }
+
+    updatedMilliSecond++;
+
+    return setTime({
+      hour: updatedHour,
+      minute: updatedMinute,
+      second: updatedSecond,
+      milliSecond: updatedMilliSecond,
+    });
+  };
+
+  const reset = () => {
+    clearInterval(interv);
+    setTime({
+      hour: 0,
+      minute: 0,
+      second: 0,
+      milliSecond: 0,
+    });
+  };
+
   return (
     <div className="main-section">
       <div className="clock-holder">
         <div className="stopwatch">
           <DisplayComponent time={time} />
-          <ButtonComponent />
+          <ButtonComponent start={start} reset={reset} />
         </div>
       </div>
     </div>
--- stop-watch/src/components/ButtonComponent.jsx
+++ stop-watch/src/components/ButtonComponent.jsx
@@ -1,56 +1,14 @@
 import React from "react";
 
-const ButtonComponent = () => {
+const ButtonComponent = ({ start, reset }) => {
   return (
     <div>
-      {props.status === 0 ? (
-        <button
-          className="stopwatch-btn stopwatch-btn-gre"
-          onClick={props.start}
-        >
-          Start
-        </button>
-      ) : (
-        ""
-      )}
-
-      {props.status === 1 ? (
-        <div>
-          <button
-            className="stopwatch-btn stopwatch-btn-red"
-            onClick={props.stop}
-          >
-            Stop
-          </button>
-          <button
-            className="stopwatch-btn stopwatch-btn-yel"
-            onClick={props.reset}
-          >
-            Reset
-          </button>
-        </div>
-      ) : (
-        ""
-      )}
-
-      {props.status === 2 ? (
-        <div>
-          <button
-            className="stopwatch-btn stopwatch-btn-gre"
-            onClick={props.resume}
-          >
-            Resume
-          </button>
-          <button
-            className="stopwatch-btn stopwatch-btn-yel"
-            onClick={props.reset}
-          >
-            Reset
-          </button>
-        </div>
-      ) : (
-        ""
-      )}
+      <button className="stopwatch-btn stopwatch-btn-gre" onClick={start}>
+        Start
+      </button>
+      <button className="stopwatch-btn stopwatch-btn-red" onClick={reset}>
+        Reset
+      </button>
     </div>
   );
 };
